ac9a6cc31db4024a4f779d03ac3947201b65efd3
smitherd9 <smitherd9@gmail.com>
2016-10-28 15:32:29 +0000

    added first test and got status code 200

--- server.js
+++ server.js
@@ -1,4 +1,12 @@
 var express = require('express');
 var app = express();
 app.use(express.static('public'));
+
+
+
+
+
+exports.app = app;
+
+
 app.listen(process.env.PORT || 8080);
---
+++ test/test-server.js
@@ -0,0 +1,22 @@
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var server = require('../server.js');
+
+var should = chai.should();
+var app = server.app;
+
+
+chai.use(chaiHttp);
+
+
+describe('chances', function() {
+    it('should return status code 200 and HTML on GET', function(done) {
+        chai.request(app)
+            .get('/')
+            .end(function(err, res) {
+                res.should.have.status(200);
+                done();
+            });
+    });
+   
+});
